2d2384def8ffd340181dc9ac58c0aa7b00992766
Christian Diener <ch.diener@gmail.com>
2021-09-13 17:12:00 -0700

    update qiime2 script

--- setup_qiime2.py
+++ setup_qiime2.py
@@ -103,8 +103,7 @@ if __name__ == "__main__":
         )
 
         run_and_check(
-            ["conda", "env", "update", "-n", "base", "--file",
-             "qiime2-2021.4-py38-linux-conda.yml"],
+            ["conda", "env", "update", "-n", "base", "--file", QIIME_YAML],
             "To activate this environment, use",
             ":mag: Installing Qiime 2. This may take a little bit.\n :clock1:",
             "could not install Qiime 2 :sob:",
@@ -121,8 +120,8 @@ if __name__ == "__main__":
         ":bar_chart: Qiime 2 command line looks good :tada:"
     )
 
-    if sys.version_info[0:2] == (3, 8):
-        sys.path.append("/usr/local/lib/python3.8/site-packages")
+    if sys.version_info[0:2] == (int(pyver[0]), int(pyver[1])):
+        sys.path.append("/usr/local/lib/python3.{}/site-packages".format(pyver[1]))
         con.log(":mag: Fixed import paths to include Qiime 2.")
 
         con.log(":bar_chart: Checking if Qiime 2 import works...")
